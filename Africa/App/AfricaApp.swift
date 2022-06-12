1c0cfeb5c6bbf256d39588517d0d3b5af07218c9
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-06-12 13:03:04 +0300

    Initial commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Alex on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Africa
+Africa app from Udemy course SwiftUI Masterclass 2022 - iOS App Development &amp; Swift
