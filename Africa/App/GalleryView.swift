f55da0710a8e001debe340f8b4b7f7c36e6fbb96
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-06-17 17:02:31 +0300

    Gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,10 +8,71 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: - Properties
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    @State private var gridColumnCount: Double = 3
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    func gridSwitch() {
+        gridLayout = Array.init(repeating: GridItem(.flexible()), count: Int(gridColumnCount))
+    }
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                
+                // Image
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // Slider
+                Slider(value: $gridColumnCount, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumnCount) { _ in
+                        gridSwitch()
+                    }
+                
+                // Grid
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: Loop
+                } //: Grid
+                .animation(.easeIn, value: UUID())
+                .onAppear {
+                    gridSwitch()
+                }
+                
+            } //: VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        } //: ScrollView
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
+
+// MARK: - Preview
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
